72638b065e43073a3e2c4672b64a253e1503d400
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-03-16 20:04:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let thankYous = []
+    for (let i = 0; i < names.length; i++){
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }
+}
